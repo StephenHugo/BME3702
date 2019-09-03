e798d6fcb51fc4b672c9c0c7ec5a252111e547d7
Stephen Arce <sarce@LABB114-00.COE>
2019-09-03 17:01:21 -0400

    updated notes for week 2

--- notes2.m
+++ notes2.m
@@ -1,18 +1,27 @@
 %% Create some data
 
 x = 1:50;
-y = x^2 + exp(0.03*x);
-z = y*x;
+y = x.^2 + -sin(1./x) + exp(0.03*x); % y(x)
+% remember your dots!
+z = y.*x; % z(x, y)
 
 %% create a blank figure
 figure
-
+% close all
+% closes all open windows
 %% Plot the data
 
 plot(x, y) % y vs. x
-
+figure
 plot(x, z) % z vs. x
 % what do you expect to see?
+
+%% Using subplots
+
+subplot(3,1,1) % 1st suplot
+plot(x, y) % y vs. x
+subplot(3,1,2) % 2nd subplot
+plot(x, z) % z vs. x
 
 %% concatenate vectors
 
@@ -22,10 +31,45 @@ yz = [y; z];
 
 %% plot m x n matrices
 
-plot(xx, yz)
-% is this correct?
+plot(xx', yz','s')
+% plot reads by column
+
+%% using get and set
+
+get(gcf) % get data on current figure
+
+set(gcf, 'Color', [0.9    0.6   0.9 ])
+
+%% using gca
+
+get(gca) % get current axes
+
+set(gca, 'Xscale', 'linear')
+set(gca, 'FontSize', 24)
+
+%% using gco
+
+% first select something in a plot
+set(gco, 'Color', 'black')
+set(gco,'ydata', log(z))
+
+%% using children of objects
+
+plot(xx', yz' , 's')
+ch = get(gca,'Children');
+set(ch(1),'markersize',12)
+set(ch(2),'markerfacecolor','g')
 
 %% images
+
+nebula1 = imread('nebula1.jpg');
+
+% three ways to display images
+figure, image( nebula1 )   % 
+figure, imshow( nebula1 )   %
+figure, imagesc( nebula1 )  %
+% be careful about the format of your images!
+
 
 %% histograms
 
